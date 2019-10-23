22d7a1e54bc47b32fbb785bc4f6595d349de070e
Leo <leoamby@gmail.com>
2019-10-23 16:41:12 +0300

    create the projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length = 30)
+    image = models.ImageField(uploads_to = 'images')
+    description = models.TextField()
