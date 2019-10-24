7e22610cd852066bccf5350cf06d7036f101df7f
Leo <leoamby@gmail.com>
2019-10-24 11:06:43 +0300

    created a projects database and changed the settings with required information

--- webly/settings.py
+++ webly/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'webly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projects',
+        'USER': 'moringa',
+        'PASSWORD': 'psql',
     }
 }
 
@@ -119,5 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
